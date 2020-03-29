0dbe35611e9879f8eebd75c6bfcdbdd5a1c98c52
Gisele Ribeiro <giselesousar@gmail.com>
2020-03-29 12:16:18 -0300

    Logon page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './styles.css'
+
+import {FiLogIn} from 'react-icons/fi'
+
+import heroesImg from '../../assets/heroes.png';
+import logoImg from '../../assets/logo.svg';
+
+export default function Logon(){
+    return(
+        <div className="Logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+
+                <form>
+                    <h1>Fça seu login</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                         Não tenho cadastro
+                    
+                    </a>
+                </form>
+            </section>
+
+            <img src={heroesImg} alt="Heroes"/>
+        </div>
+    );
+}
